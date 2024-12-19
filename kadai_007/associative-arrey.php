1ee8b1ce7843617ce25dc5dec28cce1076be5517
pikakn <akanaritamade@outlook.jp>
2024-12-19 17:01:12 +0900

    7章課題　課題フォルダ名の修正

---
+++ kadai_007/associative-arrey.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>7章課題</title>
+    </head>
+    <body>
+        <?php
+        $onion_marc = ["name" => "onion", "price" => 200, "weight" => 160];
+        print_r($onion_marc);
+        ?>
+    </body>
+</html>
